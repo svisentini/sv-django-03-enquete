01695ce00d3ae2b5b6601de0b551a2dbdcd7ad1f
Sidao <svisentini@gmail.com>
2025-02-20 22:10:05 -0300

    Lista de produtos

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Joia, TipoJoia
 # Register your models here.
 admin.site.register(Question)
+admin.site.register(Joia)
+admin.site.register(TipoJoia)
--- polls/models.py
+++ polls/models.py
@@ -20,3 +20,26 @@ class Choice(models.Model):
     #
     def __str__(self):
         return self.choice_text
+
+# ===================================================================================
+# Teste para o catalogo
+
+class TipoJoia(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Joia(models.Model):
+    codigo = models.CharField(max_length=50, unique=True)
+    descricao = models.TextField()
+    preco_compra = models.DecimalField(max_digits=10, decimal_places=2)
+    preco_venda = models.DecimalField(max_digits=10, decimal_places=2)
+    quantidade = models.PositiveIntegerField()
+    foto = models.ImageField(upload_to='joias/', blank=True, null=True)
+    tipo = models.ForeignKey(TipoJoia, on_delete=models.CASCADE)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.codigo} - {self.descricao}"
